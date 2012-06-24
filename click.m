c4f6752b29eb6302414f752b54db31f4bfee2250
Abe Stanway <abe@etsy.com>
2012-06-24 16:22:58 -0400

    Import appkit

--- click.m
+++ click.m
@@ -13,6 +13,7 @@
 
 #import <Foundation/Foundation.h>
 #import <ApplicationServices/ApplicationServices.h>
+#import <AppKit/NSEvent.h>
 
 
 int main(int argc, char *argv[]) {
@@ -33,7 +34,27 @@ int main(int argc, char *argv[]) {
   CGPoint pt;
   pt.x = x;
   pt.y = y;
+  
 
+  // Here, we find the current mouse position so we can update it relative to the
+  // new offsets
+  NSPoint mouseLoc;
+  mouseLoc = [NSEvent mouseLocation];
+
+  int diffX = pt.x - mouseLoc.x;
+  int diffY = pt.x - mouseLoc.y;
+  
+  if (diffX < 0){
+      
+  } else {
+      
+  }
+  
+  if (diffY < 0){
+      
+  } else {
+      
+  }
 
   // This is where the magic happens.  See CGRemoteOperation.h for details.
   //
@@ -44,13 +65,10 @@ int main(int argc, char *argv[]) {
   //
   // So, we feed coordinates to CGPostMouseEvent, put the mouse there,
   // then click and release.
-  //
   
   if (click == 0){
     CGPostMouseEvent( pt, 1, 1, 0 );
-  }
-  
-  if (click == 1){
+  } else {
     CGPostMouseEvent( pt, 1, 1, 1 );
     CGPostMouseEvent( pt, 1, 1, 0 );  
   }
